409f88345e9ddf168730c23377c280a1939a2d0a
armin <205030081+giwdulnimra@users.noreply.github.com>
2025-10-04 19:35:35 +0200

    included *.txt-files for working on the CMake-Part of the Graphics4D-Library

--- ws5_export/mpy_graphics4d/micropython.mk
+++ ws5_export/mpy_graphics4d/micropython.mk
@@ -1,9 +1,27 @@
-# -- CONFIG
+# PATH config
 GRAPHICS4D_DIR := $(USERMOD_DIR)
-LIBPATH1 := scr/..
+
+# original C++-Library
+SRC_USERMOD_LIB_CXX += $(GRAPHICS4D_DIR9)/lib/Graphics4D.cpp
+
+# C-Sources PSRAM-Support
+SRC_USERMOD_C += \
+	$(GRAPHICS4D_DIR)/lib/psram_tools/rp_pico_alloc.c \
+	$(GRAPHICS4D_DIR)/lib/psram_tools/psram_tool.c \
+	$(GRAPHICS4D_DIR)/lib/psram_tools/tlsf/tlsf.c
+
+# Binding/Wrapper-File
+SRC_USERMOD_CXX += $(GRAPHICS4D_DIR)/modgraphics4d.cpp
+
+# Falls zusätzliche Iclude-Pfade nötig sind:
+CXXFLAGS_USERMOD += \
+	-I$(GRAPHICS4D_DIR)/ \
+	-I$(GRAPHICS4D_DIR)/lib/ \
+	-I$(GRAPHICS4D_DIR)/lib/psram_tools/ \
+	-I$(GRAPHICS4D_DIR)/lib/psram_tools/tlsf/
 
 # --
-SRC_USERMOD_C += $(GRAPHICS4D_DIR)/modexample.c
-SRC_USERMOD_LIB_C += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.c
-SRC_USERMOD_CXX += $(GRAPHICS4D_DIR)/modexample.cpp
-SRC_USERMOD_LIB_CXX += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.cpp
+# SRC_USERMOD_C += $(GRAPHICS4D_DIR)/modexample.c
+# SRC_USERMOD_LIB_C += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.c
+# SRC_USERMOD_CXX += $(GRAPHICS4D_DIR)/modexample.cpp
+# SRC_USERMOD_LIB_CXX += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.cpp

374eb94765716c1ddb1a2fe30f028fd030224344
armin <205030081+giwdulnimra@users.noreply.github.com>
2025-10-04 20:26:49 +0200

    rearranged custom module structure, updated .mk & .cmake for upy-export

--- ws5_export/mpy_graphics4d/micropython.mk
+++ ws5_export/mpy_graphics4d/micropython.mk
@@ -2,26 +2,35 @@
 GRAPHICS4D_DIR := $(USERMOD_DIR)
 
 # original C++-Library
-SRC_USERMOD_LIB_CXX += $(GRAPHICS4D_DIR9)/lib/Graphics4D.cpp
+SRC_USERMOD_LIB_CXX += \
+	$(GRAPHICS4D_DIR9)/lib/Graphics4D.cpp
+# Binding/Wrapper-File
+SRC_USERMOD_CXX += \
+	$(GRAPHICS4D_DIR)/modgraphics4d.cpp
 
 # C-Sources PSRAM-Support
 SRC_USERMOD_C += \
-	$(GRAPHICS4D_DIR)/lib/psram_tools/rp_pico_alloc.c \
-	$(GRAPHICS4D_DIR)/lib/psram_tools/psram_tool.c \
-	$(GRAPHICS4D_DIR)/lib/psram_tools/tlsf/tlsf.c
+	$(GRAPHICS4D_DIR)/src/psram_tools/rp_pico_alloc.c \
+	$(GRAPHICS4D_DIR)/src/psram_tools/psram_tool.c \
+	$(GRAPHICS4D_DIR)/src/psram_tools/tlsf/tlsf.c
 
-# Binding/Wrapper-File
-SRC_USERMOD_CXX += $(GRAPHICS4D_DIR)/modgraphics4d.cpp
+# Assembly-Sources zur automatischen pioasm in rp2-Make
+SRC_USERMOD_LIB_ASM += \
+	$(GRAPHICS4D_DIR)/src/rgb43.pio \
+	$(GRAPHICS4D_DIR)/src/rgb50.pio \
+	$(GRAPHICS4D_DIR)/src/rgb70.pio \
+	$(GRAPHICS4D_DIR)/src/rgb90.pio \
+	$(GRAPHICS4D_DIR)/src/bus_2040.pio
 
-# Falls zusätzliche Iclude-Pfade nötig sind:
+# Falls zusätzliche Include-Pfade nötig sind:
 CXXFLAGS_USERMOD += \
 	-I$(GRAPHICS4D_DIR)/ \
-	-I$(GRAPHICS4D_DIR)/lib/ \
-	-I$(GRAPHICS4D_DIR)/lib/psram_tools/ \
-	-I$(GRAPHICS4D_DIR)/lib/psram_tools/tlsf/
+	-I$(GRAPHICS4D_DIR)/src/ \
+	-I$(GRAPHICS4D_DIR)/src/psram_tools/ \
+	-I$(GRAPHICS4D_DIR)/src/psram_tools/tlsf/
 
-# --
-# SRC_USERMOD_C += $(GRAPHICS4D_DIR)/modexample.c
-# SRC_USERMOD_LIB_C += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.c
-# SRC_USERMOD_CXX += $(GRAPHICS4D_DIR)/modexample.cpp
-# SRC_USERMOD_LIB_CXX += $(GRAPHICS4D_DIR)/$(LIBPATH1)/algorithm.cpp
+# Fonts aktivieren
+CXXFLAGS_USERMOD +=
+	-DUSE_4D_FONT2 \
+	-DUSE_4D_FONT3 \
+	-DUSE_4D_FONT4
